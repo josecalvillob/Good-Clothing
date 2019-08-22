741a0e2f46e8f01e83cef74717b614e67255c017
Jose Calvillo <josecalvillob@icloud.com>
2019-08-21 22:48:47 -0400

    added navigation of the homepage menu items using withRouter

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -12,33 +12,33 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats"
+          linkUrl: "hats"
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: "shop/jackets"
+          linkUrl: ""
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
-          linkUrl: "shop/sneakers"
+          linkUrl: ""
         },
         {
           title: "women",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
-          linkUrl: "shop/women"
+          linkUrl: ""
         },
         {
           title: "men",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
-          linkUrl: "shop/men"
+          linkUrl: ""
         }
       ]
     };
@@ -48,8 +48,8 @@ class Directory extends React.Component {
     const { sections } = this.state;
     return (
       <div className="directoryMenu">
-        {sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {sections.map(({ id, ...otherSectionsProps }) => (
+          <MenuItem key={id} {...otherSectionsProps} />
         ))}
       </div>
     );
