864c4fceb9dae07eb69c55cd453f72052da6d22c
Jose Calvillo <josecalvillob@icloud.com>
2019-09-08 17:37:57 -0400

    added redux and implemented userRuducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -6,47 +6,39 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/signInAndSignUp/signInAndSignUp.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
+  unsubscribeFromAuth = null;
   // When component mounts, get the current user and set it to our user in state
   componentDidMount() {
-    this.unsubscribe = auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-          console.log(this.state);
         });
-      } else {
-        this.setState({ currentUser: userAuth });
-        console.log(this.state);
       }
+      //setCurrentUser(userAuth);
     });
   }
 
   componentWillUnmount() {
-    this.unsubscribe();
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
         {/* let the header know if user is logged in */}
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -57,4 +49,13 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+// Passing null as the first argument because we don't need to matchStateToProps
+// since we don't need currentUser here
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
