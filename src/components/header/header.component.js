6d377da7dcf2d3e490ab1895dd4cf2b6732f54ca
Jose Calvillo <josecalvillob@icloud.com>
2019-09-09 09:04:48 -0400

    add shopping cart icon to header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,6 +4,7 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
