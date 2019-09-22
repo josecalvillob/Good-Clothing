8ee054a05d61b5d754c018564ee361fcc699f67f
Jose Calvillo <josecalvillob@icloud.com>
2019-09-22 12:10:41 -0400

    implemented redux-persist to save the session to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
+  // No need to persist user since it is being persisted by firebase
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
